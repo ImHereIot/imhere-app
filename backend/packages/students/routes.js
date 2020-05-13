6f8e81206597fe683e3ca2e89e50402f8d206a26
leomayerk <leomayerk@hotmail.com>
2020-05-12 23:24:26 -0300

    Remove: backend stay only in imhere-api repository

--- backend/packages/students/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const handlers = require('./handlers')
-
-router.get('/', auth, handlers.list);
-router.post('/', auth, handlers.create);
-router.delete('/cancel',auth, handlers.cancel)
-
-module.exports = router;
